119dcb1d2ea25136fa47136affa25f9d49ec3015
Jia Jun Yang <y_jiajun_16@hotmail.com>
2020-07-15 12:21:54 -0300

    test guardar afiliado con igual id plan

--- spec/app/app_spec.rb
+++ spec/app/app_spec.rb
@@ -4,11 +4,22 @@ describe 'App' do
   before(:each) do
     data = { 'nombre' => 'PlanJuventud', 'precio' => 100 }.to_json
     post '/planes', data
+    @plan_repository = PlanRepository.new
   end
 
   it 'deberia guardarse el plan ingresado' do
-    @repo = PlanRepository.new
-    planes = @repo.all
+    planes = @plan_repository.all
     expect(planes.length).to be 1
   end
+  # rubocop:disable RSpec/ExampleLength
+  it 'deberia guardarse el afiliado ingresado junto con su plan' do
+    plan = @plan_repository.all.first
+    id_plan = plan.id
+    afiliado_repository = AfiliadoRepository.new
+    post '/afiliados', { 'nombre': 'Juan', 'nombre_plan': 'PlanJuventud' }.to_json
+    afiliados = afiliado_repository.all
+    expect(afiliados.first.nombre).to eql 'Juan'
+    expect(afiliados.first.id_plan).to eql id_plan
+  end
+  # rubocop:enable RSpec/ExampleLength
 end
