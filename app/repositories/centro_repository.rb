5379dba358314a05238d3291ba00431ca20c6b33
PatricioIribarneCatella <patricio_14_94@hotmail.com>
2020-07-15 12:28:23 -0300

    Add CentroRepository::find method and update CentroRepository::save. Add migration

--- app/repositories/centro_repository.rb
+++ app/repositories/centro_repository.rb
@@ -1,7 +1,44 @@
 class CentroRepository
+  def initialize
+    @table_name = :centros
+  end
+
   def save(centro)
-    centro.id = 1
+    id = insert(centro)
+
+    centro.id = id
 
     centro
+  end
+
+  def find(id)
+    load_object(dataset.first!(pk_column => id))
+  end
+
+  private
+
+  def insert(a_record)
+    dataset.insert(changeset(a_record))
+  end
+
+  def dataset
+    DB[@table_name]
+  end
+
+  def pk_column
+    Sequel[@table_name][:id]
+  end
+
+  def load_object(a_record)
+    centro = Centro.new(a_record[:name])
+    centro.id = a_record[:id]
+
+    centro
+  end
+
+  def changeset(centro)
+    {
+      name: centro.nombre
+    }
   end
 end
--- spec/app/repositories/centro_repository_spec.rb
+++ spec/app/repositories/centro_repository_spec.rb
@@ -11,4 +11,10 @@ describe 'CentroRepository' do
   it 'deberia guardar el centro generando un id positivo' do
     expect(@centro.id).to be_positive
   end
+
+  it 'deberia encontrar el centro luego de haberse guardado' do
+    saved_centro = @repo.find(@centro.id)
+
+    expect(saved_centro.nombre).to eql @centro.nombre
+  end
 end
