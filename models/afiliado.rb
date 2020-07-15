eadd393655cb52b754d4a7a6f1a8e9c9e685c5b1
Jia Jun Yang <y_jiajun_16@hotmail.com>
2020-07-15 01:46:44 -0300

    test creacion afiliado sin id telegram

--- models/afiliado.rb
+++ models/afiliado.rb
@@ -1,9 +1,8 @@
 class Afiliado
   attr_accessor :id, :nombre, :id_telegram, :id_plan
 
-  def initialize(nombre, id_telegram, id_plan)
+  def initialize(nombre, id_plan)
     @nombre = nombre
-    @id_telegram = id_telegram
     @id_plan = id_plan
   end
 end
--- spec/models/afiliado_spec.rb
+++ spec/models/afiliado_spec.rb
@@ -6,11 +6,20 @@ describe 'Afiliado' do
     nombre = 'Juan'
     id_telegram = '1'
     id_plan = 10
-    afiliado = Afiliado.new(nombre, id_telegram, id_plan)
+    afiliado = Afiliado.new(nombre, id_plan)
+    afiliado.id_telegram = id_telegram
 
     expect(afiliado.nombre).to eql nombre
     expect(afiliado.id_telegram).to eql id_telegram
     expect(afiliado.id_plan).to eql id_plan
   end
   # rubocop:enable RSpec/ExampleLength
+
+  it 'deberia poder crear un afiliado sin id de telegram' do
+    nombre = 'Juan'
+    id_plan = 10
+    afiliado = Afiliado.new(nombre, id_plan)
+
+    expect(afiliado.id_telegram).to be_nil
+  end
 end
