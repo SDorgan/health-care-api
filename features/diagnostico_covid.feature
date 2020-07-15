b0caea540a207d15c483bd35c93a8aa29668ed79
SDorgan <santidorgan@gmail.com>
2020-07-15 17:08:44 -0300

    Agrego features a pasar

---
+++ features/diagnostico_covid.feature
@@ -0,0 +1,23 @@
+# language: es
+
+Característica: Diagnóstico COVID-19
+  Para que los afiliados sepan si son sospechosos de covid
+  Como administrador de la prepaga
+  Quiero que se pueda hacer un diagnóstico de covid en la app
+
+    Antecedentes:
+      Dado el plan con nombre "PlanJuventud" con costo unitario $500
+      Y se registra el plan
+
+    @wip
+    Escenario: DIAG1.1 - Diagnóstico por temperatura sin COVID
+        Dado el afiliado "Lionel Messi" afiliado a "PlanJuventud"
+        Cuando se realiza la consulta por COVID con temperatura 37
+        Entonces se obtiene que no es sospechoso
+
+    @wip
+    Escenario: DIAG2.1 - Diagnóstico por temperatura con COVID
+        Dado el afiliado "Lionel Messi" afiliado a "PlanJuventud"
+        Cuando se realiza la consulta por COVID con temperatura 40
+        Entonces se obtiene que es sospechoso
+        Y queda registrado que el afiliado "Lionel Messi" es sospechoso
---
+++ features/step_definitions/diagnostico_covid_steps.rb
@@ -0,0 +1,45 @@
+Dado('el afiliado {string} afiliado a {string}') do |nombre, plan|
+  request = {
+    'nombre' => nombre,
+    'nombre_plan' => plan
+  }
+  Faraday.post(AFILIADOS_URL, request.to_json, 'Content-Type' => 'application/json')
+end
+
+Cuando('se realiza la consulta por COVID con temperatura {int}') do |int|
+  @request = {
+    'temperatura' => int
+  }
+  @response = Faraday.post(COVID_URL, @request.to_json, 'Content-Type' => 'application/json')
+end
+
+Entonces('se obtiene que no es sospechoso') do
+  json_response = JSON.parse(@response.body)
+
+  resultado = json_response['resultado']
+  expect(@response.status).to eq 200
+  expect(resultado).not_to be 'Sospechoso'
+end
+
+Entonces('se obtiene que es sospechoso') do
+  json_response = JSON.parse(@response.body)
+
+  resultado = json_response['resultado']
+  expect(@response.status).to eq 200
+  expect(resultado).to be 'Sospechoso'
+end
+
+Entonces('queda registrado que el afiliado {string} es sospechoso') do |string|
+  @request = {
+    'afiliado' => string
+  }
+  json_afiliado = JSON.parse(@response_afiliado.body)
+  id_afiliado = json_afiliado['id']
+  URL = "#{COVID_URL}/#{id_afiliado}".freeze
+
+  @response = Faraday.get(COVID_URL, @request.to_json, 'Content-Type' => 'application/json')
+  json_response = JSON.parse(@response.body)
+
+  resultado = json_response['resultado']
+  expect(resultado).to be 'Sospechoso'
+end
