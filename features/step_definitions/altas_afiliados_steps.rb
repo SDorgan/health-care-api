8712cdf49ee291ae2f40ee5d2041ab7ff09c30a4
Jia Jun Yang <y_jiajun_16@hotmail.com>
2020-07-27 17:39:48 -0300

    RA3 - Registracion fallida por tener conyuge pasando

--- features/step_definitions/altas_afiliados_steps.rb
+++ features/step_definitions/altas_afiliados_steps.rb
@@ -49,3 +49,8 @@ Entonces('obtiene un mensaje de error no alcanza el limite minimo de edad') do
   expect(@response_afiliado.status).to eq 400
   expect(@response_afiliado.body).to eq 'no alcanza el límite mínimo de edad'
 end
+
+Entonces('obtiene un mensaje de error por tener conyuge') do
+  expect(@response_afiliado.status).to eq 400
+  expect(@response_afiliado.body).to eq 'este plan no admite conyuge'
+end
