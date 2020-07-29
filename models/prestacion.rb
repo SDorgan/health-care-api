35e1bac4dc9fbab936818fe0208b12426ce9840b
SDorgan <santidorgan@gmail.com>
2020-07-29 17:09:18 -0300

    Refactor slugs: primer test de prestacion

--- models/prestacion.rb
+++ models/prestacion.rb
@@ -1,8 +1,15 @@
 class Prestacion
-  attr_accessor :id, :nombre, :costo, :centros
+  attr_accessor :id, :nombre, :slug, :costo, :centros
 
   def initialize(nombre, costo)
     @nombre = nombre
     @costo = costo
+    @slug = sluggify(nombre)
+  end
+
+  private
+
+  def sluggify(string)
+    string.downcase.tr('àáäâãèéëẽêìíïîĩòóöôõùúüûũñç ', 'aaaaaeeeeeiiiiiooooouuuuunc_')
   end
 end
