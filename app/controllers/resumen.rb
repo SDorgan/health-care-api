303f2a6f0572544827c2c6798a616e4c6c986f09
PatricioIribarneCatella <patricio_14_94@hotmail.com>
2020-08-02 10:43:44 -0300

    Minor change in ResumenController

--- app/controllers/resumen.rb
+++ app/controllers/resumen.rb
@@ -3,8 +3,10 @@ require_relative '../errors/id_not_afiliado_error'
 HealthAPI::App.controllers :resumen do
   get :index do
     id_telegram = request.params['id']
-    repo_afiliados = AfiliadoRepository.new
-    resumen = Resumen.new(repo_afiliados.find_by_telegram_id(id_telegram),
+
+    afiliado = AfiliadoRepository.new.find_by_telegram_id(id_telegram)
+
+    resumen = Resumen.new(afiliado,
                           VisitaMedicaRepository.new,
                           CompraMedicamentosRepository.new)
 
