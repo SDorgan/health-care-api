fe9b1c24cf799b3eb75f50a5ac161e02a1799312
SDorgan <santidorgan@gmail.com>
2020-08-04 20:07:07 -0300

    Centro Cercano: paso tercer test CentroService

--- spec/domain/services/centro_service_spec.rb
+++ spec/domain/services/centro_service_spec.rb
@@ -10,19 +10,9 @@ describe 'CentroService' do
       -58.544412
     end
 
-    let(:centro_lejano) do
-      centro = Centro.new('Hospital Alemán', -35.446324, -59.544443)
-
-      centro
-    end
-
-    let(:centro_cercano) do
-      centro = Centro.new('Hospital Suizo', -34.446354, -58.544443)
-
-      centro
-    end
-
     before(:each) do
+      @centro_cercano = Centro.new('Hospital Suizo', -34.446354, -58.544443)
+      @centro_lejano = Centro.new('Hospital Alemán', -35.446324, -59.544443)
       @repo = CentroRepository.new
       @calculador_distancia = instance_double('CalculadorDistancia')
     end
@@ -38,7 +28,7 @@ describe 'CentroService' do
     end
 
     it 'debería devolver el único elemento existente' do # rubocop:disable RSpec/ExampleLength, RSpec/MultipleExpectations, Metrics/LineLength
-      centro = @repo.save(centro_lejano)
+      centro = @repo.save(@centro_lejano)
       respuesta = { 'distancias': [107.562], 'direcciones': ['Ruta Nacional 205'] }
 
       allow(@calculador_distancia).to receive(:obtener_direcciones_a_punto).and_return(respuesta)
@@ -52,15 +42,15 @@ describe 'CentroService' do
       expect(centros.first.direccion).to eq 'Ruta Nacional 205'
     end
 
-    xit 'debería devolver el elemento más cercano' do # rubocop:disable RSpec/ExampleLength
-      _centro = @repo.save(centro_lejano)
-      centro_cercano = @repo.save(centro_cercano)
+    it 'debería devolver el elemento más cercano' do # rubocop:disable RSpec/ExampleLength
+      _centro = @repo.save(@centro_lejano)
+      centro_cercano = @repo.save(@centro_cercano)
       respuesta = { 'distancias': [107.562, 0.165], 'direcciones': ['Ruta Nacional 205', '2282 Avenida del Libertador'] } # rubocop:disable Metrics/LineLength
 
       allow(@calculador_distancia).to receive(:obtener_direcciones_a_punto).and_return(respuesta)
 
       centro_service = CentroService.new(CentroRepository.new, @calculador_distancia)
-      centros = centro_service.buscar(@latitud_busqueda, @longitud_busqueda)
+      centros = centro_service.buscar(latitud: latitud_busqueda, longitud: longitud_busqueda)
 
       expect(centros.length).to be 1
       expect(centros.first.id).to be centro_cercano.id
