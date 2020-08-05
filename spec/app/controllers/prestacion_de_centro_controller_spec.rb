359923a325aedbb35e57946f2237450afec7510e
Jia Jun Yang <y_jiajun_16@hotmail.com>
2020-08-05 00:19:36 -0300

    agregado halt 403 cuando no tiene api key, con test

--- spec/app/controllers/prestacion_de_centro_controller_spec.rb
+++ spec/app/controllers/prestacion_de_centro_controller_spec.rb
@@ -30,41 +30,46 @@ describe 'PrestacionDeCentroController' do
   end
 
   it 'deberia devolver un JSON con prestaciones como clave' do
-    get "/centros/#{@centro.id}/prestaciones"
+    get "/centros/#{@centro.id}/prestaciones", {}, 'HTTP_API_KEY' => API_KEY
     last_response.body.include?('prestaciones')
   end
 
   it 'deberia devolver las prestaciones cargadas para un centro' do
-    post "/centros/#{@centro.id}/prestaciones", { 'prestacion': @otra_prestacion.id }.to_json
-    get "/centros/#{@centro.id}/prestaciones"
+    post "/centros/#{@centro.id}/prestaciones", { 'prestacion': @otra_prestacion.id }.to_json, 'HTTP_API_KEY' => API_KEY
+    get "/centros/#{@centro.id}/prestaciones", {}, 'HTTP_API_KEY' => API_KEY
     response = JSON.parse(last_response.body)
 
     expect(response['prestaciones'].length).to eq 2
   end
 
   it 'deberia devolver ok al hacer el POST' do
-    post "/centros/#{@centro.id}/prestaciones", { 'prestacion': @otra_prestacion.id }.to_json
+    post "/centros/#{@centro.id}/prestaciones", { 'prestacion': @otra_prestacion.id }.to_json, 'HTTP_API_KEY' => API_KEY
     last_response.body.include?('ok')
   end
 
   it 'deberia devolver error al hacer el POST con prestación inexistente' do
     pres_id = (@prestacion.id + @otra_prestacion.id + 1)
-    post "/centros/#{@centro.id}/prestaciones", { 'prestacion': pres_id }.to_json
+    post "/centros/#{@centro.id}/prestaciones", { 'prestacion': pres_id }.to_json, 'HTTP_API_KEY' => API_KEY
     expect(last_response.status).to be 404
     expect(last_response.body).to eq 'La prestación pedida no existe'
   end
 
   it 'deberia devolver error al hacer el POST con un centro inexistente' do
-    post "/centros/#{@centro.id + @otro_centro.id + 1}/prestaciones", { 'prestacion': @prestacion.id }.to_json
+    post "/centros/#{@centro.id + @otro_centro.id + 1}/prestaciones", { 'prestacion': @prestacion.id }.to_json, 'HTTP_API_KEY' => API_KEY
     expect(last_response.status).to be 404
     expect(last_response.body).to eq 'El centro pedido no existe'
   end
 
   it 'deberia devolver error al hacer el POST con una prestación repetida en el centro' do
-    post "/centros/#{@centro.id}/prestaciones", { 'prestacion': @otra_prestacion.id }.to_json
-    post "/centros/#{@centro.id}/prestaciones", { 'prestacion': @otra_prestacion.id }.to_json
+    post "/centros/#{@centro.id}/prestaciones", { 'prestacion': @otra_prestacion.id }.to_json, 'HTTP_API_KEY' => API_KEY
+    post "/centros/#{@centro.id}/prestaciones", { 'prestacion': @otra_prestacion.id }.to_json, 'HTTP_API_KEY' => API_KEY
 
     expect(last_response.status).to be 400
     expect(last_response.body).to eq 'El centro ya presenta esa prestación'
+  end
+
+  it 'deberia devolver 403 al no tener api key' do
+    post "/centros/#{@centro.id}/prestaciones", { 'prestacion': @otra_prestacion.id }.to_json
+    expect(last_response.status).to be 403
   end
 end
