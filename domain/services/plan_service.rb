7f2baf9efe1810ea6fb745b3b45eb2396082ddfe
PatricioIribarneCatella <patricio_14_94@hotmail.com>
2020-08-05 11:38:58 -0300

    Refactor: move logic from Plan::create class method into PlanService::registrar method

--- domain/services/plan_service.rb
+++ domain/services/plan_service.rb
@@ -16,4 +16,42 @@ class PlanService
   rescue PlanNoEncontrado
     raise PlanInexistenteError
   end
+
+  def registrar(params = {})
+    limite_visitas = params['limite_cobertura_visitas']
+    costo_copago = params['copago']
+    porcentaje_medicamentos = params['cobertura_medicamentos']
+
+    cobertura_visitas = if !limite_visitas.nil?
+                          CoberturaVisita.new(limite_visitas,
+                                              costo_copago)
+                        else
+                          CoberturaVisitaInfinita.new(costo_copago)
+                        end
+
+    cobertura_medicamentos = CoberturaMedicamentos.new(porcentaje_medicamentos)
+
+    data = format_data(params)
+
+    data[:cobertura_visitas] = cobertura_visitas
+    data[:cobertura_medicamentos] = cobertura_medicamentos
+
+    plan = Plan.new(data)
+
+    @repo_planes.save(plan)
+  end
+
+  private
+
+  def format_data(params)
+    data = {}
+    data[:nombre] = params['nombre']
+    data[:costo] = params['costo']
+    data[:edad_minima] = params['edad_minima']
+    data[:edad_maxima] = params['edad_maxima']
+    data[:cantidad_hijos_maxima] = params['cantidad_hijos_maxima']
+    data[:conyuge] = params['conyuge']
+
+    data
+  end
 end
